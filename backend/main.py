5daafe0c223d30c33aa33ab397270c406960c0e1
suman <suman.gaire@nimble-cr.com>
2024-09-07 11:28:32 +0545

    create websocket for real time chat

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from api.v1 import user_login, user_register
 from db_connection import Base, engine
 from fastapi import FastAPI
+from websocket import socket
 
 app = FastAPI()
 
 app.include_router(user_register.router)
 app.include_router(user_login.router)
+app.include_router(socket.router)
 
 
 @app.on_event("startup")
